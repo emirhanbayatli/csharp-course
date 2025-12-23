3f07bc57ca7e7d0d925d08d9002c44c8954d090f
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2025-12-23 20:28:36 +0300

    Add project files.

---
+++ 09_DatabaseProject/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** C# Veri Tabanli Urun-Kategori Bilgi Sistemi ****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Urunler");
+            Console.WriteLine("3-Siparisler");
+            Console.WriteLine("4-Cikis Yap");
+
+            Console.Write("Lutfen Getirmek istediginiz tablo numarasini giriniz: ");
+            tableNumber = Console.ReadLine();
+
+            Console.WriteLine("-------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=.;initial Catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * From TblCategory", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            connection.Close();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString());
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 10_DataBaseCrud/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DataBaseCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
